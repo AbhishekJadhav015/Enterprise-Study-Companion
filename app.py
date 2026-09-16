f7fe1448563b2cba0edc124820200003c664e6e9
Abhishek Jadhav <abhishekjadhav0015@gmail.com>
2026-09-16 11:04:43 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Educational Study Companion")
+st.write("Welcome to the Educational Study Companion! This app is designed to help you with your studies by providing resources, study tips, and interactive tools.")
---
+++ src/ingestion/chunker.py
@@ -0,0 +1,16 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from typing import List
+
+class DocumentChunker:
+    "splits documents logically while maintaining context overlap"
+    
+    def __init__(self , chunk_size: int = 1000, chunk_overlap: int = 200) :
+        self.splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        # the heierarchy of the text splitter is as follows: paragraphs -> lines -> sentences -> words
+        
+    def split(self, documents: List[Document]) -> List[Document]:
+        print(f"chunking {len(documents)} pages....")
+        chunks = self.splitter.split_documents(documents)
+        print(f"chunked {len(chunks)} contextual chunks")
+        return chunks
